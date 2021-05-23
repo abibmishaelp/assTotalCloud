0646bb2ff9495309780b7aa4f6a0917f6f34155e
Abib Mishael P <abibmishael1@gmail.com>
2021-05-23 19:35:49 +0530

    sort button added

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # assTotalCloud
 An assigment from TotalCloud Inc.
+
+****FOR WINDOWS *****
+To run the project
+
+1.  Download the project to any local directiory.
+2.  Go to the project path in node.js command prompt or GIT Bash command prompt and run "npm i" it will install required packages.
+3.  Run "npm start" and open a browser and access to "http://localhost:3000/" for the execution.
+4.  Exit the proccess and close the command prompt to exit the project.
+
+****FOR LINUX *****
+To run the project
+
+1.  Download the project to any local directiory.
+2.  Go to the project path in terminal and run "npm i" it will install required packages.
+3.  Run "npm start" and open a browser and access to "http://localhost:3000/" for the execution.
+4.  Exit the proccess and close the terminal to exit the project.
+
+Thank You
+
+
+@Abib Mishael 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 let async = require("async");
 var express = require('express');
 var axios = require('axios');
+const e = require("express");
 var router = express.Router();
+let totalUser;
 
 /* GET home page. */
 router.get('/', async (req, res) => {
   let userData;
   try {
+    // console.log(req.params.sort);
+    // let funcName = req.params.sort;
     userData = await axios({
       method: 'get',
       url: 'https://reqres.in/api/users?delay=3',
@@ -14,6 +18,9 @@ router.get('/', async (req, res) => {
     })
       .then((response) => {
         console.log("Data", response.data);
+        // let result = sorting_Function(response.data.data, funcName);
+        // console.log(result);
+        totalUser = response.data.total;
         return response.data;
       })
       .catch((error) => {
@@ -22,7 +29,11 @@ router.get('/', async (req, res) => {
       });
   } catch (e) {
     console.log("TryCatch Error@24", e);
-    return e;
+    return res.render('error', {
+      message: error.message,
+      status: error.status,
+      stack: eror.stack
+    });
   }
   finally {
     console.log(userData.data);
@@ -39,20 +50,32 @@ router.get('/getDetails/:id', async (req, res, next) => {
   try {
     console.log(req.params.id);
     let userId = req.params.id;
-    userDetails = await axios({
-      method: 'get',
-      url: `https://reqres.in/api/users/${userId}`,
-      responseType: 'json'
-    }).then((response) => {
-      console.log("Data", response.data);
-      return response.data;
-    }).catch((error) => {
-      console.log("Error@50", error);
-      return error;
-    });
+    if (totalUser <= userId || totalUser === userId ) {
+      return res.render('error', {
+        message: "User Not Found",
+        status: "NOT FOUND",
+        stack: "404"
+      });
+    } else {
+      userDetails = await axios({
+        method: 'get',
+        url: `https://reqres.in/api/users/${userId}`,
+        responseType: 'json'
+      }).then((response) => {
+        console.log("Data", response.data);
+        return response.data;
+      }).catch((error) => {
+        console.log("Error@50", error);
+        return error;
+      });
+    }
   } catch (e) {
     console.log("error@54", e);
-    return e;
+    return res.render('error', {
+      message: error.message,
+      status: error.status,
+      stack: eror.stack
+    });
   }
   finally {
     console.log(userDetails);
@@ -64,5 +87,63 @@ router.get('/getDetails/:id', async (req, res, next) => {
   }
 });
 
+// async function sorting_Function(userData, type) {
+//   switch (type) {
+//     case "none":
+//       return userData;
+//       break;
+
+//     case "last":
+//       // function compare(a, b) {
+//       //   if (a.last_name < b.last_name) {
+//       //     return -1;
+//       //   }
+//       //   if (a.last_name > b.last_name) {
+//       //     return 1;
+//       //   }
+//       //   return 0;
+//       // }
+//       // return userData.sort(compare);
+//       userData.sort((a, b) => {
+//         let fa = a.last_name.toLowerCase(),
+//           fb = b.last_name.toLowerCase();
+
+//         if (fa < fb) {
+//           return -1;
+//         }
+//         if (fa > fb) {
+//           return 1;
+//         }
+//         return 0;
+//       });
+//       break;
+
+//     case "first":
+//       // function compare(a, b) {
+//       //   if (a.first_name < b.first_name) {
+//       //     return 1;
+//       //   }
+//       //   if (a.first_name > b.first_name) {
+//       //     return -1;
+//       //   }
+//       //   return 0;
+//       // }
+//       // return userData.sort(compare);
+//       userData.sort((a, b) => {
+//         let fa = a.first_name.toLowerCase(),
+//           fb = b.first_name.toLowerCase();
+
+//         if (fa < fb) {
+//           return -1;
+//         }
+//         if (fa > fb) {
+//           return 1;
+//         }
+//         return 0;
+//       });
+//       break;
+//   }
+
+// }
 
 module.exports = router;
