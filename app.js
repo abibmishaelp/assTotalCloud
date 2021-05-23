eb7e7cbaa42b593e1d8db6cb815e9ff124f7c86d
Abib Mishael P <abibmishael1@gmail.com>
2021-05-23 16:54:34 +0530

    changes are made as per assignment

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(express.static('public'));
 app.use(express.static('views/images')); 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,67 @@
+let async = require("async");
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Assigment for TotalCloud Inc' });
+router.get('/', async (req, res) => {
+  let userData;
+  try {
+    userData = await axios({
+      method: 'get',
+      url: 'https://reqres.in/api/users?delay=3',
+      responseType: 'json'
+    })
+      .then((response) => {
+        console.log("Data", response.data);
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("error@20", error);
+        return error;
+      });
+  } catch (e) {
+    console.log("TryCatch Error@24", e);
+    return e;
+  }
+  finally {
+    console.log(userData.data);
+    res.render('index', {
+      title: 'Assigment for TotalCloud Inc',
+      subTitle: "Assignment",
+      users: userData.data
+    });
+  }
 });
 
-router.get('/getDetails', function (req, res, next) {
-  res.render('userDetails', {
-    title: 'User Details'
-  });
+router.get('/getDetails/:id', async (req, res, next) => {
+  let userDetails;
+  try {
+    console.log(req.params.id);
+    let userId = req.params.id;
+    userDetails = await axios({
+      method: 'get',
+      url: `https://reqres.in/api/users/${userId}`,
+      responseType: 'json'
+    }).then((response) => {
+      console.log("Data", response.data);
+      return response.data;
+    }).catch((error) => {
+      console.log("Error@50", error);
+      return error;
+    });
+  } catch (e) {
+    console.log("error@54", e);
+    return e;
+  }
+  finally {
+    console.log(userDetails);
+    res.render('userDetails', {
+      title: 'UserDetails',
+      subTitle: "User",
+      userDetails: userDetails.data
+    });
+  }
 });
 
 
